77b9873e359e4983bae7329705a50f1d99dbd8fc
Adam Malcolm <malcolm282@outlook.com>
2021-01-27 14:21:59 +0000

    Removed some code

--- script.py
+++ script.py
@@ -1,11 +1,5 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
